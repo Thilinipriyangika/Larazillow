471f12efa48d70c2ec713706119828e03b646472
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-03-08 09:51:33 +0530

    First commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Index</div>
+    <div>
+    <link href="/hello">Show Page</link>
+    </div>
+</template>
+
+<script setup>
+
+import {Link} from '@inertiajs/inertia-vue3'
+
+</script>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Show</div>
+    <link href="/"> Main Page </link>
+</template>
+
+<script setup>
+
+import {Link} from '@inertiajs/inertia-vue3'
+
+</script>
