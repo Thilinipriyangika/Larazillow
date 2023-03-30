f9088ba1dd6573d0977db1ffd4a65aeed768c59f
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-03-30 13:02:27 +0530

    Adding Pagination done

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,35 +10,43 @@ class ListingController extends Controller
 {
     public function __construct()
     {
-        $this->authorizeResource(Listing::class,'listing');
-
+        $this->authorizeResource(Listing::class, 'listing');
     }
+
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function index(): \Inertia\Response|\Inertia\ResponseFactory
+    public function index()
     {
         return inertia(
             'Listing/Index',
             [
-                'listings' => Listing::all()
+                'listings' => Listing::orderByDesc('created_at')
+                    ->paginate(10)
             ]
         );
     }
 
     /**
      * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function create(): \Inertia\Response|\Inertia\ResponseFactory
+    public function create()
     {
-        //$this->authorize('create',Listing::class);
+        // $this->authorize('create', Listing::class);
         return inertia('Listing/Create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request): \Illuminate\Http\RedirectResponse
+    public function store(Request $request)
     {
         $request->user()->listings()->create(
             $request->validate([
@@ -49,24 +57,27 @@ class ListingController extends Controller
                 'code' => 'required',
                 'street' => 'required',
                 'street_nr' => 'required|min:1|max:1000',
-                'price' => 'required|integer|min:1|max:20000000'
-
+                'price' => 'required|integer|min:1|max:20000000',
             ])
         );
 
         return redirect()->route('listing.index')
-            ->with('success','Listing was created!');
+            ->with('success', 'Listing was created!');
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show(Listing $listing): \Inertia\Response|\Inertia\ResponseFactory
+    public function show(Listing $listing)
     {
-        //if (Auth::user()->cannot('view',$listing)){
-         //   abort(403);
-       // }
-        //$this->authorize('view',$listing);
+        // if (Auth::user()->cannot('view', $listing)) {
+        //     abort(403);
+        // }
+        // $this->authorize('view', $listing);
+
         return inertia(
             'Listing/Show',
             [
@@ -75,9 +86,6 @@ class ListingController extends Controller
         );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Listing $listing)
     {
         return inertia(
@@ -88,27 +96,23 @@ class ListingController extends Controller
         );
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Listing $listing)
     {
         $listing->update(
             $request->validate([
-            'beds' => 'required|integer|min:0|max:20',
-            'baths' => 'required|integer|min:0|max:20',
-            'area' => 'required|integer|min:15|max:1500',
-            'city' => 'required',
-            'code' => 'required',
-            'street' => 'required',
-            'street_nr' => 'required|min:1|max:1000',
-            'price' => 'required|integer|min:1|max:20000000'
-
-        ])
+                'beds' => 'required|integer|min:0|max:20',
+                'baths' => 'required|integer|min:0|max:20',
+                'area' => 'required|integer|min:15|max:1500',
+                'city' => 'required',
+                'code' => 'required',
+                'street' => 'required',
+                'street_nr' => 'required|min:1|max:1000',
+                'price' => 'required|integer|min:1|max:20000000',
+            ])
         );
 
         return redirect()->route('listing.index')
-            ->with('success','Listing was changed!');
+            ->with('success', 'Listing was changed!');
     }
 
 
@@ -117,6 +121,6 @@ class ListingController extends Controller
         $listing->delete();
 
         return redirect()->back()
-            ->with('success','Listing was deleted!');
+            ->with('success', 'Listing was deleted!');
     }
 }
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,41 +1,18 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <div>
-                <Link
-                    :href="route('listing.show', {listing: listing.id})"
-                >
-                    <Price :price="listing.price" class="text-2xl font-bold" />
-                    <ListingSpace :listing="listing" class="text-lg" />
-                    <ListingAddress :listing="listing" class="text-gray-500" />
-                </Link>
-            </div>
-            <div>
-                <Link
-                    :href="route('listing.edit', {listing: listing.id})"
-                >
-                    Edit
-                </Link>
-            </div>
-            <div>
-                <Link
-                    :href="route('listing.destroy', {listing: listing.id})"
-                    method="DELETE" as="button"
-                >
-                    Delete
-                </Link>
-            </div>
-        </Box>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing"/>
+    </div>
+
+    <div v-if="listings.data.length" className="w-full flex justify-center mt-8 mb-8">
+        <Pagination :links="listings.links"/>
     </div>
 </template>
 
 <script setup>
-import {Link} from '@inertiajs/inertia-vue3'
-import ListingAddress from '@/Components/ListingAddress.vue'
-import Box from '@/Components/UI/Box.vue'
-import ListingSpace from '@/Components/ListingSpace.vue'
-import Price from '@/Components/Price.vue'
+import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
+
 defineProps({
-    listings: Array,
+    listings: Object,
 })
 </script>
