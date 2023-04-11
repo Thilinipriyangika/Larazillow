4938873f655abf73754b1585f030eb4c38fcbaf6
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-04-11 20:18:16 +0530

    filtering offers done

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -11,7 +11,7 @@
             </div>
         </Box>
 
-        <div v-else class="md:col-span-7 items-center">
+        <div v-else class="md:col-span-7 flex flex-col gap-4">
             <Offer
                 v-for="offer in listing.offers"
                 :key="offer.id" class="mb-4"
@@ -20,13 +20,16 @@
             />
         </div>
 
-        <Box class="md:col-span-5">
-            <template #header>Basic Info</template>
-            <Price :price="listing.price" class="text-2xl font-bold"/>
+        <div class="md:col-span-5">
+            <Box class="md:col-span-5">
+                <template #header>Basic Info</template>
+                <Price :price="listing.price" class="text-2xl font-bold"/>
 
-            <ListingSpace :listing="listing" class="text-lg"/>
-            <ListingAddress :listing="listing" class="text-gray-500"/>
-        </Box>
+                <ListingSpace :listing="listing" class="text-lg"/>
+                <ListingAddress :listing="listing" class="text-gray-500"/>
+            </Box>
+        </div>
+
     </section>
 </template>
 
