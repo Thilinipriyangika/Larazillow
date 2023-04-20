c01e48d05edbe6987fe0f8e8bc192d2d6b39370c
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-04-20 20:57:08 +0530

    User verification route done

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\UserAccountController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -45,12 +46,24 @@ Route::post('login', [AuthController::class, 'store'])
 Route::delete('logout', [AuthController::class, 'destroy'])
     ->name('logout');
 
+Route::get('/email/verify', function () {
+    return inertia('Auth/VerifyEmail');
+})
+->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect()->route('listing.index')
+        ->with('success', 'Email was verified!');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
 
 Route::prefix('realtor')
     ->name('realtor.')
-    ->middleware('auth')
+    ->middleware('auth','verified')
     ->group(function () {
         Route::name('listing.restore')
             ->put(
