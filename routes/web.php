9fc62aa868860395054d788f18cdfbd3fc7a1da7
Thilini Priyangika <85427990+Thilinipriyangika@users.noreply.github.com>
2023-04-20 21:34:17 +0530

    Resending email done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
@@ -57,6 +58,12 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
     return redirect()->route('listing.index')
         ->with('success', 'Email was verified!');
 })->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('success', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
